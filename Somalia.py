2a50812906b0ba12af4e1b70fa5a7a2657ba5673
escwademographics <138754610+escwademographics@users.noreply.github.com>
2024-02-06 12:16:17 +0200

    Update Somalia.py

--- Somalia.py
+++ Somalia.py
@@ -20,7 +20,7 @@ st.write('<style>div.block-container{padding-top:0rem;}</style>', unsafe_allow_h
 
 left_co, cent_co,last_co = st.columns(3)
 with last_co:
-    st.image("https://i2.wp.com/ummah-futures.net/wp-content/uploads/2019/12/%D8%A7%D9%84%D9%84%D8%AC%D9%86%D8%A9-%D8%A7%D9%84%D8%A7%D9%82%D8%AA%D8%B5%D8%A7%D8%AF%D9%8A%D8%A9-%D9%88%D8%A7%D9%84%D8%A7%D8%AC%D8%AA%D9%85%D8%A7%D8%B9%D9%8A%D8%A9-%D9%84%D8%BA%D8%B1%D8%A8%D9%8A-%D8%A2%D8%B3%D9%8A%D8%A7-1.jpg?w=500&ssl=1")
+    st.image("https://www.unescwa.org/sites/default/files/images/flags/Flag_of_Somalia.svg")
 with left_co:
     st.image("https://m.media-amazon.com/images/I/51P-e1yYpGL._AC_UF894,1000_QL80_.jpg")
 st.image("title.PNG")
